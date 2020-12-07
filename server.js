8de3c80f633fe6aced366060cae2898cd6d1fa7e
Khaildoran <sgermscheid@gmail.com>
2020-12-06 18:40:00 -0600

    mine file static

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const mineCMDS = require('mine.lua')
 
 app.use(cors());
-
+app.use(express.static('assets'));
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,9 +12,6 @@ app.get('/hello',  (req, res) => {
     res.send('HelloWorld')
 });
 
-app.get('/mine', (req, res) => {
-    res.send(mineCMDS)
-})
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
