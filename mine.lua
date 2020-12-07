e25469782b56e75e8949303fd452a9230e51f31f
Khaildoran <sgermscheid@gmail.com>
2020-12-06 18:32:49 -0600

    mine cmd

---
+++ mine.lua
@@ -0,0 +1,13 @@
+function mineForward(num)
+    turtle.refuel()
+    local i = 0
+    while i <= num do 
+        turtle.dig()
+        turtle.forward()
+        turtle.dig()
+        print(i)
+        i = i + 1
+end
+end
+
+mineForward(20)
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const mineCMDS = require('mine.lua')
 
 app.use(cors());
 
@@ -11,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.get('/hello',  (req, res) => {
     res.send('HelloWorld')
 });
+
+app.get('/mine', (req, res) => {
+    res.send(mineCMDS)
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
