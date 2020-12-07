a04e5b116980f6168a359beebe2a9f562feab659
Zach Germscheid <zachgermscheid@Rogue02.local>
2020-12-07 10:33:57 -0600

    Creating New turtle squence

---
+++ assets/mineExcavate.lua
@@ -0,0 +1,26 @@
+function mineForward(num)
+    turtle.refuel()
+    local i = 0
+    while i <= num do 
+        turtle.dig()
+        turtle.forward()
+        turtle.dig()
+        print(i)
+        i = i + 1
+    end
+    turtle.digUp()
+    turtle.up()
+    turtle.turnRight()
+    turtle.turnRight()
+
+    i = 0
+
+    while i <= num do 
+        turtle.dig()
+        turtle.forward()
+        turtle.dig()
+        print(i)
+        i = i + 1
+    end
+end
+mineForward(20)
