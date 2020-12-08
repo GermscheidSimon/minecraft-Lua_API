ae8b81cfc1f5cbb981e8164e72c3bd079b286f42
zach Germscheid <default11.110@gmail.com>
2020-12-08 14:56:33 -0600

    Adding Detect, removed break chest.

--- assets/mineForward.lua
+++ assets/mineForward.lua
@@ -4,6 +4,64 @@ local z = 0 -- side-side
 
 local square = 0 -- size of mine area
 
+local notItems = {
+    "minecraft:dirt",
+    "minecraft:stone",
+    "minecraft:gravel",
+    "minecraft:chest",
+    "minecraft:flowing_lava"
+      } 
+   
+  function compDown()
+  local succes, data = turtle.inspectDown()
+   if succes then
+    if data.name == notItems[1] then
+      elseif data.name == notItems[2] then
+        elseif data.name == notItems[3] then  
+          elseif data.name == notItems[4] then
+            for i = 1, 27 do
+              turtle.suckDown()
+            end
+            turtle.digDown()
+              elseif data.name == notItems[5] then
+                turtle.select(15)
+                turtle.placeDown()
+              else
+            turtle.digDown()      
+        end
+     end
+  end
+   
+  function compUp()
+  local succes, data = turtle.inspectUp()
+    if succes then
+      if data.name == notItems[1] then
+        elseif data.name == notItems[2] then
+          elseif data.name == notItems[3] then
+            elseif data.name == notItems[4] then
+              for i = 1, 27 do
+                turtle.suckUp()
+              end
+              turtle.digUp()
+              elseif data.name== notItems[5] then
+                turtle.select(15)
+                turtle.placeUp()
+            else
+            turtle.digUp()     
+         end    
+      end
+  end
+   
+  function compare()
+    compDown()
+    compUp()  
+  end
+
+  function fuel()
+    if turtle.getFuelLevel() < 50 then
+      turtle.refuel()
+    end
+  end
 
 function mineForward(num)
     
@@ -17,16 +75,15 @@ function mineForward(num)
 
             write (n)
         
-            turtle.refuel()
-        
             local i = 0
         
-            while i <= num do 
+            while i <= num do
+                compare() 
                 turtle.dig()
                 turtle.forward()
-                turtle.dig()
                 i = i + 1
             end
+            compare()
             turtle.digUp()
             turtle.up()
             turtle.turnRight()
@@ -35,12 +92,13 @@ function mineForward(num)
             i = 0
 
             while i <= num do 
+                compare()
                 turtle.dig()
                 turtle.forward()
-                turtle.dig()
                 i = i + 1
             end
 
+            compare()
             turtle.down()
             turtle.turnLeft()
             turtle.dig()
@@ -87,10 +145,13 @@ function mineForward(num)
             turtle.down()
             end
         end
+        compare()
         turtle.digDown()
         turtle.down()
 
         y = y + 1
+        
+        fuel()
     end
 
 end
